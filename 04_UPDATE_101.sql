dc3d1860d80cb80ae852b16fdd698c3147634f92
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-11-04 23:43:29 -0500

    T-SQL -Update 101
    T-SQL DELETE statements basics

--- 04_UPDATE_101.sql
+++ 04_UPDATE_101.sql
@@ -1,7 +1,34 @@
-UPDATE dbo.Person
-SET eyeColor = 'brown'
-WHERE firstName = 'George'
+-- ################
+-- UPDATE
+-- ################
 
 UPDATE dbo.Person
 SET person.eyeColor = 'honey'
 WHERE person.Age = 11
+
+UPDATE dbo.Person
+SET person.eyeColor = 'honey'
+WHERE person.Age = 11
+
+UPDATE dbo.Person
+SET person.eyeColor = 'light brown'
+WHERE person.firstName = 'Alexander'
+
+UPDATE dbo.Person
+SET person.eyeColor = 'blue'
+WHERE person.firstName = 'Patricio'
+
+-- The following statement will update all of the table rows
+UPDATE dbo.Person
+SET person.eyeColor = 'N/A'
+
+-- Updating several fields at once
+UPDATE dbo.Person
+SET person.eyeColor = 'green', person.Age = 19
+WHERE person.firstName = 'Patricio'
+
+
+-- ################
+-- DELETE
+-- ################
+
