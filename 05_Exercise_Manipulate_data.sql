c7471e8b3df41f899bba566144a1c4a8298a3d00
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-11-05 00:10:54 -0500

    T-SQL Exercise Manipuate data completed as requested

--- 05_Exercise_Manipulate_data.sql
+++ 05_Exercise_Manipulate_data.sql
@@ -1,7 +1,62 @@
--- Recreating Table from FirstDB
--- using INSERT SELECT Clause
+-- Recreating Table from FirstDB using INSERT SELECT Clause
 INSERT INTO dbo.Person([firstName],[Age])
 SELECT firstName, age
 FROM FirstDB.dbo.People
 
+-- Updating eyeColor field for each row
+UPDATE dbo.Person
+SET Person.eyeColor = 'black'
+WHERE Person.firstName = 'Mike'
+
+UPDATE dbo.Person
+SET Person.eyeColor = 'brown'
+WHERE Person.firstName = 'Rudolph'
+
+UPDATE dbo.Person
+SET Person.eyeColor = 'green'
+WHERE Person.firstName = 'Mario'
+
+UPDATE dbo.Person
+SET Person.eyeColor = 'blue'
+WHERE Person.firstName = 'Stewart'
+
+UPDATE dbo.Person
+SET Person.eyeColor = 'N/A'
+WHERE Person.firstName = 'Peter'
+
+UPDATE dbo.Person
+SET Person.eyeColor = 'N/A'
+WHERE Person.firstName = 'Bruce'
+
+UPDATE dbo.Person
+SET Person.eyeColor = 'light brown'
+WHERE Person.firstName = 'Wally'
+
+-- ######################################
+--You should practice the following
+
+--Updating a single column in a single row
+UPDATE dbo.Person
+SET Person.Age = 27
+WHERE Person.firstName = 'Wally'
+
+--Updating a several columns at once -- 
+UPDATE dbo.Person
+SET Person.firstName = 'Wally West', Person.Age = 25
+WHERE Person.firstName = 'Wally'
+
+--Updating several rows at once
+UPDATE dbo.Person
+SET Person.eyeColor = 'No data available'
+WHERE Person.eyeColor = 'N/A'
+
+--Deleting a single row
+DELETE
+FROM dbo.Person
+WHERE Person.firstName = 'Rudolph'
+
+--Deleting several rows
+DELETE
+FROM dbo.Person
+WHERE Person.eyeColor = 'No data available'
 
