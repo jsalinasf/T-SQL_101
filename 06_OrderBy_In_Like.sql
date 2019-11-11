db59f6d45f0960c387e4e25d5543854edc4d5733
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-11-10 20:26:27 -0500

    T-SQL ORDER BY, IN, LIKE statements basics

--- 06_OrderBy_In_Like.sql
+++ 06_OrderBy_In_Like.sql
@@ -7,3 +7,10 @@ ORDER BY LouisvilleCrime.ZipCode
 SELECT LouisvilleCrime.Description, LouisvilleCrime.CrimeType
 FROM dbo.LouisvilleCrime
 ORDER BY LouisvilleCrime.CrimeType
+
+-- LIKE Statement
+-- % is a wildcard operator
+SELECT *
+FROM dbo.LouisvilleCrime
+-- Pay ttention to the use of the % wildcard operator
+WHERE LouisvilleCrime.Description LIKE 'FLEEING%'
