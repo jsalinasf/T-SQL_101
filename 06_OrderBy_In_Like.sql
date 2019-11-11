44818298a8744b03f792c59dc25470a640c84a94
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-11-10 20:41:23 -0500

    T-SQL ORDER BY, IN, LIKE statements - ORDER Clause goes ALWAYS after the WHERE CLAUSE

--- 06_OrderBy_In_Like.sql
+++ 06_OrderBy_In_Like.sql
@@ -41,3 +41,10 @@ FROM dbo.LouisvilleCrime
 -- You can use more than one %
 WHERE LouisvilleCrime.Description LIKE '%SHOPLIFTING%'
 
+
+-- The ORDER BY clause goes after the WHERE clause. 
+SELECT LouisvilleCrime.Description, LouisvilleCrime.CrimeType, LouisvilleCrime.ZipCode
+FROM LouisvilleCrime
+WHERE LouisvilleCrime.CrimeType = 'THEFT/LARCENY'
+ORDER BY LouisvilleCrime.ZipCode
+
