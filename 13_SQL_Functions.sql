777f6022a6e41d442ba8361c0a08e15279eb2628
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-11-21 19:29:55 -0500

    SIX Weeks SQL - Functions 101, additional functions

--- 13_SQL_Functions.sql
+++ 13_SQL_Functions.sql
@@ -1,3 +1,10 @@
+-- STR Converts INT or FLOAT into STRINGS
+-- Pay attention to how STR specifies the format
+-- desired characteres '4' - This includes the decimal point
+-- desired decimals '2'
+SELECT STR(ABV * 100, 4, 2) + '%'
+FROM dbo.Beer
+
 -- CAPITALIZE Strings
 SELECT UPPER(Beers.Style)
 FROM Beers
@@ -15,3 +22,46 @@ FROM dbo.Beers
 SELECT LEN(Beers.Name) AS NameLength, Beers.Name
 FROM dbo.Beers
 ORDER BY NameLength
+
+-- MAX(): Returns the highest value in dataset
+SELECT MAX(Beers.ABV)
+FROM dbo.Beers
+
+-- SUBQUERY
+SELECT Beers.Name
+FROM dbo.Beers
+WHERE
+Beers.ABV = (
+		--Insert query here
+		SELECT MAX(Beers.ABV)
+		FROM dbo.Beers
+		)
+
+-- COUNT
+SELECT COUNT(*)
+FROM dbo.Beers
+
+-- AVERAGE
+SELECT AVG(Beers.ABV)
+FROM dbo.Beers
+
+-- ROUND
+SELECT ROUND(AVG(Beers.ABV), 2)
+FROM dbo.Beers
+
+-- SUBSTRING (AKA: splice)
+-- INDEX starts in '1')
+-- 2nd PArameter is the length of the substring you are searching for
+SELECT SUBSTRING('PIZZA', 1, 3)
+SELECT(SUBSTRING('SQLNOOBZ', 4, 3))
+
+-- FUNCTIONS can be used in the WHERE CLAUSE as well
+SELECT Beers.Name
+FROM dbo.Beers
+WHERE SUBSTRING(Beers.Name, 1, 4) = 'amer'
+
+-- DATEPART extracts specific element (hour, day, minute) from DATES
+-- Example: Return the Description of crimes that occurred in hour 23
+SELECT Description
+FROM dbo.LouisvilleCrime
+WHERE DATEPART(hour, DateOccurred) = '23'
