4561c5010ffe515cc4c71812923c834c683e0622
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-11-28 21:34:05 -0500

    SIX Weeks SQL - GROUP BY 102 - Aggregate functions and group by

--- 15_SQL_GroupBy_102.sql
+++ 15_SQL_GroupBy_102.sql
@@ -11,3 +11,19 @@ GROUP BY SalaryData.Department
 SELECT SalaryData.Department, COUNT(SalaryData.EmployeeName)
 FROM dbo.SalaryData
 GROUP BY SalaryData.Department
+
+-- Let's improve our last query. Give the aggregate column an alias of EmployeeCount and ORDER the results by it.
+SELECT SalaryData.Department, COUNT(SalaryData.EmployeeName) AS EmployeeCount
+FROM dbo.SalaryData
+GROUP BY SalaryData.Department
+ORDER BY EmployeeCount
+
+-- Find the SUM of the AnnualRate of each Department
+SELECT SalaryData.Department, SUM(SalaryData.AnnualRate)
+FROM dbo.SalaryData
+GROUP BY SalaryData.Department
+ 
+ -- Group by Department and find the highest AnnualRate for each
+SELECT SalaryData.Department, MAX(SalaryData.AnnualRate)
+FROM dbo.SalaryData
+GROUP BY SalaryData.Department
