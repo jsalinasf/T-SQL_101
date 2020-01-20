a56e4e1c87422b1cc6feb73834d32a11f937a6d7
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-01-20 11:46:25 -0500

    SQL - Six Weeks - Foreign Keys 101

--- 25_Foreign_Key.sql
+++ 25_Foreign_Key.sql
@@ -4,3 +4,11 @@ CREATE TABLE PropertyTaxes (
     PropertyAddress varchar(500),
     FOREIGN KEY (PropertyAddress) REFERENCES HousesForSale(Address)
 );
+
+
+
+CREATE TABLE PropertyTaxes (
+    PropertyTax money,
+    PropertyAddress varchar(500),
+    FOREIGN KEY (PropertyAddress) REFERENCES HousesForSale(Address)
+);
