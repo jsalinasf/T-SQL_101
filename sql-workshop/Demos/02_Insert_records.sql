6e426921576919a9469984a0d0b0e5207195f1af
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-03-23 22:13:10 -0500

    SQL WorkShop PacktPub - SQL Basics commands - Insert Into

---
+++ sql-workshop/Demos/02_Insert_records.sql
@@ -0,0 +1,13 @@
+USE studentdemo;
+
+INSERT INTO student (StudentID, StudentName,grade,age,course) 
+VALUES ('S001','Mike Donovan','A',36,'Computer Science');
+
+SELECT * from student;
+
+INSERT INTO Student (StudentID, StudentName, grade, age, course) VALUES
+('S002', 'Frank Solomon', 'B', 35, 'Physics'), 
+('S003', 'Rachana Karia', 'B', 36, 'Electronics'),
+('S004', 'Ambika Prashanth', 'C', 35, 'Mathematics');
+
+SELECT * from student;
