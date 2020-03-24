2ed580d662afd3aa8aed73d03bf043d1b3ae134f
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-03-23 22:00:49 -0500

    SQL WorkShop PacktPub - exercise 101 completed

---
+++ sql-workshop/Exercises/exercise-101.sql
@@ -0,0 +1,19 @@
+-- drop database PACKT_ONLINE_SHOP;
+
+create database PACKT_ONLINE_SHOP;
+
+use PACKT_ONLINE_SHOP;
+
+create table Customers
+(
+	FirstName varchar(50),
+    MiddleName varchar(50),
+    LastName varchar(50),
+    HomeAddress varchar(250),
+    Email varchar(200),
+    Phone varchar(50),
+    Notes varchar(250)
+);
+
+select * from customers;
+
