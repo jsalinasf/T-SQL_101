63b9a4d8944f719b0782cfd5ea36922d42e29d19
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-03-25 18:18:22 -0500

    SQL Workshop - Exercise Insert Into

---
+++ sql-workshop/Exercises/exercise-201.sql
@@ -0,0 +1,16 @@
+create database EMPLOYEE;
+
+use EMPLOYEE;
+
+create table Department
+(
+	departmentNo INT Primary Key,
+    departmentName VARCHAR(20) NOT NULL,
+    departmentLoc VARCHAR(50) NOT NULL
+);
+
+INSERT INTO Department (departmentNo, departmentName, departmentLoc)
+VALUE
+(1,'Engg','Texas');
+
+SELECT * from Department;
