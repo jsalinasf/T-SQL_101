af2f90c3fbe6f24f0a9be2c38ab74ded01c2b5ae
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-04-01 23:47:17 -0500

    SQL Workshop - Activity 201 completed - ALTER columns default value

--- sql-workshop/Activities/activity-201.sql
+++ sql-workshop/Activities/activity-201.sql
@@ -1,11 +1,17 @@
 USE packt_online_shop;
 
+SHOW COLUMNS FROM products;
+
+ALTER TABLE products ALTER ProductCategoryID SET DEFAULT 1;
+
+SHOW COLUMNS FROM products;
+
 SELECT * from products;
 
+INSERT INTO products(SupplierID,ProductName,NetRetailPrice,AvailableQuantity,WholesalePrice,UnitKGWeight,Notes)
+VALUES
+(2,'Pancake batter',5.99,105,3.99,0.25,'fresh pancacke batter - activity 201'),
+(1,'Breakfast cereal',5.99,120,3.99,0.50,'Nestle cereal - activity 201'),
+(5,'Siracha sauce',5.99,57,3.99,1.20,'Home made sauce - activity 201');
 
-
--- INSERT INTO products(SupplierID,ProductName,NetRetailPrice,AvailableQuantity,WholesalePrice,UnitKGWeight,Notes)
--- VALUES
--- (2,'Pancake batter',5.99,105,3.99,0.25,'fresh pancacke batter - activity 201'),
--- (1,'Breakfast cereal',5.99,120,3.99,0.50,'Nestle cereal - activity 201'),
--- (5,'Siracha sauce',5.99,57,3.99,1.20,'Home made sauce - activity 201');
+SELECT * from products;
