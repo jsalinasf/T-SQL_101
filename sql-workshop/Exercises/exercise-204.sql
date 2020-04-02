f9d4f1c87d2eeac59220ddddf2453a439570bc99
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-04-01 23:06:03 -0500

    SQL Workshop - ALTER command 102

--- sql-workshop/Exercises/exercise-204.sql
+++ sql-workshop/Exercises/exercise-204.sql
@@ -14,3 +14,36 @@ WHERE departmentNo = 5;
 
 SELECT * FROM employee.department;
 
+ALTER TABLE department AUTO_INCREMENT = 3;
+
+INSERT INTO department (departmentName, departmentLoc)
+VALUES
+('Sales','LV');
+
+SELECT * FROM employee.department;
+
+ALTER TABLE department AUTO_INCREMENT = 1;
+
+INSERT INTO department (departmentName, departmentLoc)
+VALUES
+('IT','LA');
+
+SELECT * FROM employee.department;
+
+DELETE FROM department
+WHERE departmentNo = 5;
+
+INSERT INTO department (departmentName, departmentLoc)
+VALUES
+('IT','MI');
+
+ALTER TABLE department AUTO_INCREMENT = 1;
+
+INSERT INTO department (departmentName, departmentLoc)
+VALUES
+('IT','TX');
+
+SELECT * FROM employee.department;
+
+DELETE FROM department
+WHERE departmentNo > 4;
