d45e6cc2c1a39d22d6d26d24f9472d3f4b414e85
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-04-01 23:29:37 -0500

    SQL Workshop - starting Activity 201

---
+++ sql-workshop/Activities/activity-201.sql
@@ -0,0 +1,11 @@
+USE packt_online_shop;
+
+SELECT * from products;
+
+
+
+-- INSERT INTO products(SupplierID,ProductName,NetRetailPrice,AvailableQuantity,WholesalePrice,UnitKGWeight,Notes)
+-- VALUES
+-- (2,'Pancake batter',5.99,105,3.99,0.25,'fresh pancacke batter - activity 201'),
+-- (1,'Breakfast cereal',5.99,120,3.99,0.50,'Nestle cereal - activity 201'),
+-- (5,'Siracha sauce',5.99,57,3.99,1.20,'Home made sauce - activity 201');
