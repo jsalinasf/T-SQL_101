b7978b52d7e8d5a6556a1e8adfee1f4a115f59ad
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-04-12 21:28:53 -0500

    SQL Workshop - Building Table relatinship 102

--- sql-workshop/Exercises/exercise-301.sql
+++ sql-workshop/Exercises/exercise-301.sql
@@ -8,12 +8,24 @@ DROP TABLE IF EXISTS department;
 
 CREATE TABLE IF NOT EXISTS department
 (
-	dno INT NOT NULL,
+	dno INT PRIMARY KEY,
     dname VARCHAR(30) UNIQUE NOT NULL,
-    dlocation VARCHAR(30) UNIQUE NOT NULL
+    dlocation VARCHAR(30) UNIQUE NOT NULL    
 );
 
-SELECT * FROM department
+CREATE TABLE IF NOT EXISTS employee
+(
+	eNo char(4) PRIMARY KEY,
+    eName VARCHAR(30) NOT NULL,
+    eJob VARCHAR(30) NOT NULL,
+    eManager char(4),
+    eJoinDate TIMESTAMP NOT NULL,
+    eGender CHAR(1),
+    eSalary DECIMAL(8,2) DEFAULT 0,
+    eCommission DECIMAL(8,2) DEFAULT 0,
+    eDeptNo INT 
+);
 
+SHOW TABLES IN employeedemo;
 
-
+SHOW COLUMNS FROM department;
