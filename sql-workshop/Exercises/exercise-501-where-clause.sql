bb4dbfa0df485bb2a047ff5f6be3f66ad9605dbb
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-05-19 19:30:55 -0500

    SQL - Between Clause 101

--- sql-workshop/Exercises/exercise-501-where-clause.sql
+++ sql-workshop/Exercises/exercise-501-where-clause.sql
@@ -9,3 +9,11 @@ SHOW COLUMNS FROM products;
 SELECT ProductName AS 'High-Value Products'
 FROM Products
 WHERE NetRetailPrice > 14.99;
+
+SELECT ProductName AS 'High-Value Products'
+FROM Products
+WHERE NetRetailPrice >= 14.99;
+
+SELECT ProductName AS 'High-Value Products'
+FROM Products
+WHERE NetRetailPrice BETWEEN 14.99 AND 1000000;
