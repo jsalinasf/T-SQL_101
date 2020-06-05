1309156aabfab87e3c118b3c73488c89f98eb239
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-06-04 21:43:20 -0500

    SQL workshop - Subquery exercise 700 fixed minor bugs

--- sql-workshop/Exercises/exercise-700-subquery-01.sql
+++ sql-workshop/Exercises/exercise-700-subquery-01.sql
@@ -9,9 +9,20 @@ SHOW COLUMNS FROM products;
 SHOW COLUMNS FROM orderitems;
 
 -- Suppose a Packt manager wants a list of products that have zero sales.
+
+-- Using a SUBQUERY
 SELECT products.ProductID, products.ProductName, products.ProductCategoryID
 FROM products
 WHERE ProductID NOT IN (
 	SELECT ProductID
     FROM orderitems
 );
+
+-- Using a regular query
+SELECT products.ProductID, products.ProductName, products.ProductCategoryID
+FROM products 
+LEFT OUTER JOIN orderitems
+ON products.ProductID = orderitems.PRODUCTID
+WHERE orderitems.ProductID IS NULL
+ORDER BY products.ProductID;
+	
