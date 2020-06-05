85c9ca5d6c1f9934563cc5734d9cbbe1b1b19091
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-06-04 21:55:10 -0500

    SQL workshop - Subquery exercise 701 completed

---
+++ sql-workshop/Exercises/exercise-701-subquery.sql
@@ -0,0 +1,11 @@
+USE packt_online_shop;
+
+SHOW TABLES;
+
+SHOW COLUMNS FROM orders;
+
+SHOW COLUMNS FROM orderitems;
+
+-- In this exercise, we will use a subquery to find the orders (as OrderID values) 
+-- for the orders with no related rows in the OrderItems table
+
