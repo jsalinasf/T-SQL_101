95afd7c0de820c16fb9c884d05a7fac0e308fd37
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-06-04 22:11:27 -0500

    SQL workshop - Activity 701, using a subquery completed

--- sql-workshop/Activities/activity-701.sql
+++ sql-workshop/Activities/activity-701.sql
@@ -1,3 +1,27 @@
 -- The Packt Online Shop management team needs to know the product category name 
 -- for the habanero peppers product. You are asked to find the required 
 -- information. Use a subquery to retrieve the requested data
+
+USE packt_online_shop;
+
+SHOW TABLES;
+SHOW COLUMNS FROM products;
+SHOW COLUMNS FROM productcategories;
+
+-- Using a regular Query
+SELECT products.ProductName, productcategories.ProductCategoryName
+FROM products
+INNER JOIN productcategories
+ON products.ProductCategoryID = productcategories.ProductCategoryID
+WHERE products.ProductName = 'habanero peppers';
+
+-- Using a Subquery
+SELECT productcategories.ProductCategoryName
+FROM productcategories
+WHERE productcategories.ProductCategoryID IN (
+	SELECT products.ProductCategoryID 
+    FROM products
+    WHERE products.ProductName= 'habanero peppers'
+);
+
+
