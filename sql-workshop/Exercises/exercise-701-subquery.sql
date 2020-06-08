58dd620460d5322804bef500d5793ad66c4b86ee
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-06-07 19:49:11 -0500

    SQL workshop - Subquery fixed

--- sql-workshop/Exercises/exercise-701-subquery.sql
+++ sql-workshop/Exercises/exercise-701-subquery.sql
@@ -9,3 +9,16 @@ SHOW COLUMNS FROM orderitems;
 -- In this exercise, we will use a subquery to find the orders (as OrderID values) 
 -- for the orders with no related rows in the OrderItems table
 
+-- Using a SUBQUERY
+SELECT orders.OrderID, orders.CustomerID, orders.OrderNumber, orders.OrderDate
+FROM orders
+WHERE orders.OrderID NOT IN (
+	SELECT orderitems.OrderID FROM orderitems
+);
+
+-- Using a Regular Query
+SELECT orders.OrderID, orders.CustomerID, orders.OrderNumber, orders.OrderDate
+FROM orders
+LEFT OUTER JOIN orderitems
+ON orders.OrderID = orderitems.OrderID
+WHERE orderitems.OrderID IS NULL;
