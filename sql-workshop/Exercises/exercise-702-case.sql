fe9d9b282642bffc1e1e79ab8d6ec34716bc626b
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-06-07 20:10:18 -0500

    SQL workshop - Exercise 702 Case

--- sql-workshop/Exercises/exercise-702-case.sql
+++ sql-workshop/Exercises/exercise-702-case.sql
@@ -1,6 +1,17 @@
 USE packt_online_shop;
 
-SELECT products.productName, products.WholesalePrice, products.NetRetailPrice, 'Price Point' AS 'Price Point', products.UnitKGWeight
+SELECT 
+	products.productName,
+	products.WholesalePrice,
+    products.NetRetailPrice,
+    CASE
+		WHEN products.NetRetailPrice <= 24.99 THEN 'Cheap'
+        WHEN products.NetRetailPrice > 24.99 AND products.NetRetailPrice <= 79.99 THEN 'Mid Price'
+        WHEN products.NetRetailPrice > 79.99 AND products.NetRetailPrice <= 2499.99 THEN 'Expensive'
+        ELSE 'Very Expensive'
+	END
+    AS 'Price Point',
+    products.UnitKGWeight
 FROM products
-ORDER BY products.ProductName;
+ORDER BY products.productName;
 
