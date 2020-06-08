9f532f678e212f68ac5aba402da34c5b5cef8240
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-06-07 21:17:53 -0500

    SQL Workshop - Views 102

--- sql-workshop/Exercises/exercise-703-view-102.sql
+++ sql-workshop/Exercises/exercise-703-view-102.sql
@@ -1,16 +1,24 @@
 USE packt_online_shop;
 
+CREATE VIEW PACKT_VIEW_1
+AS
 SELECT 
 	customers.CustomerID,
 	CONCAT(customers.FirstName,' ',customers.LastName) AS 'Customer Name',
     orders.OrderDate,
     products.ProductID,
     products.ProductName,
-    
+    orderitems.UnitPrice,
+    orderitems.Quantity,
+    orderitems.Quantity * orderitems.UnitPrice AS 'PerProductSpending'
 FROM customers
 	INNER JOIN orders
     ON customers.CustomerID = orders.CustomerID
     INNER JOIN orderitems
     ON orders.OrderID = orderitems.OrderID
     INNER JOIN products
-    ON orderitems.ProductID = products.ProductID
+    ON orderitems.ProductID = products.ProductID;
+    
+SELECT CustomerID, OrderDate, ProductID, PerProductSpending
+FROM PACKT_VIEW_1
+WHERE PerProductSpending > 14.99;
