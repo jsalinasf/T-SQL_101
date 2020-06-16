7b2f2d65c5e28444e1ff9b716facca0df89f142e
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-06-15 20:42:47 -0500

    SQL Workshop - MySQL Triggers 103

--- sql-workshop/Exercises/exercise-804-exercise01-trigger-CALL.sql
+++ sql-workshop/Exercises/exercise-804-exercise01-trigger-CALL.sql
@@ -1,15 +1,6 @@
-INSERT INTO 	Products(ProductCategoryID, SupplierID,
-						ProductName, ProductImage, NetRetailPrice,
-						AvailableQuantity, WholesalePrice,
-						UnitKGWeight, Notes)
+INSERT INTO Products(ProductCategoryID, SupplierID,	ProductName, ProductImage, NetRetailPrice, AvailableQuantity, WholesalePrice, UnitKGWeight, Notes)
+VALUES(3, 3, 'peanut butter', NULL, 3.79, 1000, 2.69, 0.75, 'caution: high calorie');
 
-				VALUES(3, 3, 'peanut butter', NULL, 3.79, 1000, 2.69,
-						0.75, 'caution: high calorie');
-
-				SELECT	P.*
-				FROM	Products P
-				WHERE	P.SupplierID = 3;
-                
--- DELETE 
--- FROM products
--- WHERE ProductID = 12;
+SELECT	S.*
+FROM	Suppliers S
+WHERE	S.SupplierID = 3;
--- sql-workshop/Exercises/exercise-804-exercise02-trigger-CALL.sql
+++ sql-workshop/Exercises/exercise-804-exercise02-trigger-CALL.sql
@@ -1,6 +1,6 @@
-INSERT INTO	OrderItems(OrderID, ProductID, Quantity,
-							UnitPrice, Discount, Notes)
-					VALUES(1, 6, 12, 4.49, 0.00, NULL);
-					SELECT	C.BalanceNotes
-					FROM	Customers C
-					WHERE	C.CustomerID = 2;
+INSERT INTO	OrderItems(OrderID, ProductID, Quantity, UnitPrice, Discount, Notes)
+VALUES(1, 6, 12, 4.99, 0.00, NULL);
+
+SELECT	*
+FROM	Customers C
+WHERE	C.CustomerID = 2;
